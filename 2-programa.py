768cfdf24ed6af497d67065064ce9b1a00266174
Daniel Rojas <danielalejandro.rojas@nxp.com>
2023-06-02 15:45:15 -0600

    termine la sesion 3

--- 2-programa.py
+++ 2-programa.py
@@ -1,8 +1,17 @@
-precioDeManzanas:int = 10
-cantidaDeManzanas:int = 5
+precioDeManzanas:int = int(input("Ingresa el precio de las manzanas: "))
+cantidaDeManzanas:int = int(input("Cuantas manzanas vendieron: "))
+
 print("Vas a pagar")
 # soy un comentario
 """
     comentario multi linea
     """
-print(precioDeManzanas*cantidaDeManzanas)
+    
+ # CONCATENACION   
+print("las manzanas estas en : " + str(precioDeManzanas) +"y fueron : " + str(cantidaDeManzanas))
+print("las manzanas estas en : " , precioDeManzanas , "y fueron : " + str(cantidaDeManzanas))
+print(f"las manzanas estas en : {precioDeManzanas} y fueron : {cantidaDeManzanas}")
+
+
+print("y fueron : " + str(cantidaDeManzanas))
+print("total a pagar : " + str(precioDeManzanas*cantidaDeManzanas))
---
+++ 2.1programa.py
@@ -0,0 +1,8 @@
+# nombre = input("Ingresa tu nombre: ")
+nombre = 'yair'
+numero1 = 2
+numero2 = "2"
+print("nombre")
+print('nombre')
+print(nombre*3)
+print(numero1 + numero2) 
