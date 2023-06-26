b4e917f144b7a6a9fd8faedfc4f22425eb165f4c
Daniel Rojas <danielalejandro.rojas@nxp.com>
2023-06-26 14:25:48 -0600

    conexion con base de datos

--- python MYSQL/codigoPropio1.py
+++ python MYSQL/codigoPropio1.py
@@ -3,25 +3,30 @@ opcionBaseDeDatos = 1
 if opcionBaseDeDatos == 1 :
     #Forma local
     host="localhost"
-    user="l20j45"
-    passwd="localTest"
+    user="l20j45" # root
+    passwd="localTest" # ""
     port = 3306
+    database='mini-siiau'
+    
 elif opcionBaseDeDatos == 2 :
     #Forma remota
     host="142.44.163.242"
-    user="Alumno"
+    user="Alumno01"
     passwd="AlumnoPython1@."
     port = 4000
+    database='mini-siiau'
 
 try:
 	conexion = mysql.connector.connect(
     host=host,
     user=user,
     passwd=passwd,
-    port=port
+    port=port,
+    database=database
 )
 	print("Conexión correcta")
 except mysql.connector.Error as err:
 	print("Ocurrió un error al conectar: ", err)
 finally:
+    print("conexion closed")
     conexion.close()
